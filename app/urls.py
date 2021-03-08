9c03fbe8c0472facabfc41dbd21242b415a1c025
hugo <hugo.lavergne@edu.devinci.fr>
2021-03-08 14:19:46 +0100

    new design and route logic

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('devs/', views.devs, name='devs'),
+    path('jobs/', views.jobs, name='jobs'),
     path('job/<int:job_id>/', views.details, name='job_details'),
     path('admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import get_object_or_404, render
 
 def index(request):
     jobs = Job.objects.all()
+    devs = Developer.objects.all()
     return render(request, 'app/index.html', {
-        "jobs": jobs
+        "jobs": jobs,
+        "devs": devs
     })
 
 def details(request, job_id):
@@ -16,6 +18,12 @@ def details(request, job_id):
     
 def devs(request):
     devs = Developer.objects.all()
-    return render(request, 'app/devs.html', {
+    return render(request, 'app/devs/index.html', {
         "devs": devs
+    })
+
+def jobs(request):
+    jobs = Job.objects.all()
+    return render(request, 'app/jobs/index.html', {
+        "jobs": jobs
     })
